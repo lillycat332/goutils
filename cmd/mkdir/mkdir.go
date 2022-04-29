358eacc656a4f9784d64a0138fb31b7773a32b82
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-04-29 12:04:39 +0100

    Add mkdir.go

---
+++ cmd/mkdir/mkdir.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// Mkdir creates a new directory with the specified name and permission bits.
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println("Usage: mkdir <dir>")
+		os.Exit(1)
+	} else {
+		err := os.Mkdir(os.Args[1], 0755)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
