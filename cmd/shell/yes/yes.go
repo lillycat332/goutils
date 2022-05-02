e4c7fac30999a4f48805df4f1fae66d3ec0ae308
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-05-02 11:41:46 +0100

    comment yes

--- cmd/shell/yes/yes.go
+++ cmd/shell/yes/yes.go
@@ -5,17 +5,20 @@ import (
 	"strings"
 )
 
+// Set the buffer size as a constant.
 const bufsize = 1024 * 1024
 
 func main() {
 	y := "y\n"
 
+	// If any arguments are passed, use them as the output, otherwise use y.
 	if len(os.Args) > 1 {
 		y = strings.Join(os.Args[1:], " ") + "\n"
 	}
 
 	buffer := strings.Repeat(y, bufsize/len(y))
 
+	// yyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
 	for {
 		os.Stdout.Write([]byte(buffer))
 	}
